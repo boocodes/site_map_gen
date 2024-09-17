b49c6d68eb3149b414943ed4214456073ef34c17
Nick Felker <felker.nick@bk.ru>
2024-09-17 15:00:28 +0600

    added xml, json, csv parser

--- index.php
+++ index.php
@@ -6,6 +6,6 @@
     $pages_arr = array(["loc"=>"test.ru", "lastmod"=>"12.10.2002", "priority"=>"high", "changefreq"=>"2"], ["loc"=>"hi.ru", "lastmod"=>"09.12.2002", "priority"=>"high", "changefreq"=>"2"], ["loc"=>"main.ru", "lastmod"=>"02.12.2003", "priority"=>"high", "changefreq"=>"1"]);
 
     
-    $obj = new Map($pages_arr, "xml", "./");
+    $obj = new Map($pages_arr, "json", "./");
 
     $obj->generate();
--- src/mapgen.php
+++ src/mapgen.php
@@ -3,7 +3,7 @@
 class Map
 {
     private $pages_arr;
-    private $file;
+    private $map;
     private $map_file_type;
     private $gen_file_path;
     function __construct($pages_arr = [], $map_file_type = "json", $gen_file_path = "./")
@@ -11,12 +11,11 @@ class Map
         $this->pages_arr = $pages_arr;
         $this->map_file_type = $map_file_type;
         $this->gen_file_path = $gen_file_path;
-        // $this->file = fopen(__DIR__ . $this->gen_file_path, 'w');
+        
     }
 
     public function generate()
     {
-
         switch ($this->map_file_type) {
             case "json":
                 $this->json_map_generate();
@@ -28,28 +27,33 @@ class Map
                 $this->xml_map_generate();
                 break;
         }
+        $file = file_put_contents($this->gen_file_path . "/sitemap." . $this->map_file_type, $this->map);
     }
 
     private function xml_map_generate() 
     {
-        $map = "<urlset xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\nxmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\"\nxsi:schemaLocation=\"https://www.sitemaps.org/schemas/sitemap/0.9\nhttp://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd\">";
+        $this->map = "<urlset xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\nxmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\"\nxsi:schemaLocation=\"https://www.sitemaps.org/schemas/sitemap/0.9\nhttp://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd\">";
         foreach($this->pages_arr AS $first_key => $array) {
-            $map .= "\n<url>\n";
+            $this->map .= "\n<url>\n";
             foreach($array AS $key => $value)
-                 $map .= "    <" . $key . ">" . $value . "</" . $key . ">" . "\n";
-            $map .= "\n</url>";
+                 $this->map .= "    <" . $key . ">" . $value . "</" . $key . ">" . "\n";
+            $this->map .= "\n</url>";
         }
-
-        echo $map;
     }
 
     private function json_map_generate() 
     {
-           
+        $this->map = json_encode($this->pages_arr);
     }
 
     private function csv_map_generate() 
     {
-        $map = "loc;lastmod;priority;changefreq";
+        $this->map = "loc;lastmod;priority;changefreq";
+        foreach($this->pages_arr AS $first_key => $array) {
+            $this->map .= "\n";
+            foreach($array AS $key => $value)
+                 $this->map .= $value . ";";
+            $this->map = substr($this->map, 0, -1);
+        } 
     }
 };
