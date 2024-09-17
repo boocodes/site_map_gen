47de7f633ef0bbe4499bfefe83f8bf3dd5569156
Nick Felker <felker.nick@bk.ru>
2024-09-17 14:27:18 +0600

    finished base xml parser

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+    include_once "./src/mapgen.php";
+
+
+    $pages_arr = array(["loc"=>"test.ru", "lastmod"=>"12.10.2002", "priority"=>"high", "changefreq"=>"2"], ["loc"=>"hi.ru", "lastmod"=>"09.12.2002", "priority"=>"high", "changefreq"=>"2"], ["loc"=>"main.ru", "lastmod"=>"02.12.2003", "priority"=>"high", "changefreq"=>"1"]);
+
+    
+    $obj = new Map($pages_arr, "xml", "./");
+
+    $obj->generate();
